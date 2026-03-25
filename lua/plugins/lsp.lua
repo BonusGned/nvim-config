489b7725b67a43b5369a54407ef9277981dd739c
boncho <manjarobespectacled@gmail.com>
2026-03-25 14:21:10 +0300

    fix: name

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -87,7 +87,7 @@ return {
                 group = vim.api.nvim_create_augroup("UserLspConfig", {}),
                 callback = function(ev)
                     -- Enable completion triggered by <c-x><c-o>
-                    vim.bo[ev.buf].omnifunc = "v:lua.vm.lsp.omnifunc"
+                    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
                     local opts = { buffer = ev.buf }
                     -- vim.keymap.set("n", "gd", vim.lsp.buf.declaration, opts)
